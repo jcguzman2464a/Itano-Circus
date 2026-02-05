81e2d2738a9fc463afdb5bda160389fe826c1121
jcguzman2464a <jcguzman2464a@gmail.com>
2026-02-05 16:07:19 -0600

    cuarta version 1.3

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'itano-circus-v2'; // Cambia el v2 cada vez que actualices el código
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json'
+];
+
+// Instalar y guardar en caché
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting(); // Fuerza a que el nuevo SW se active de inmediato
+});
+
+// Limpiar cachés antiguos
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Estrategia: Ir a la red primero, si falla usar caché
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
